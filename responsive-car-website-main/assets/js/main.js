ba6149cdff8537d9a2cb4566a57027445a1a2783
Bianca Veleanu <63341026+BiancaVVV@users.noreply.github.com>
2023-07-09 22:46:37 +0300

    Add files via upload

---
+++ responsive-car-website-main/assets/js/main.js
@@ -0,0 +1,55 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== POPULAR SWIPER ===============*/
+
+let swiperPopular = new Swiper(".popular__container", {
+    
+    loop: true,
+    spaceBetween: 24,
+    slidePerView: 'auto',
+    grabCursor: true,
+    
+    
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+
+    breakpoints: {
+
+        600:{
+            slidesPerView: 2,
+        },
+       
+        768: {
+            slidesPerView: 3,
+           
+        },
+        1024: {
+          
+            spaceBetween: 48,
+        },
+    },
+});
+
+
+/*=============== MIXITUP FILTER FEATURED ===============*/
+
+
+/* Link active featured */ 
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
